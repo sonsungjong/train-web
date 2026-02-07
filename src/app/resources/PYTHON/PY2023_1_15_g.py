0fb9728e968a441ee6dfd61f0d746ce8dccb4e20
sonsungjong <tbxmtbfm@naver.com>
2026-02-08 05:12:07 +0900

    ㅇㄴㅁㅇ

--- src/app/resources/PYTHON/PY2023_1_15_g.py
+++ src/app/resources/PYTHON/PY2023_1_15_g.py
@@ -1,10 +1,11 @@
-s = {'Korea', 'China', 'Japan'}
+data = {'사과': 100, '바나나': 200, '딸기': 300}
 
-s.add('USA')
-s.add('China')
-s.remove('Japan')
-s.update({'Korea', 'France'})
+data['파인애플'] = 400
+data.update({'바나나': 500})
+data.pop('딸기')
+data['망고'] = 600
+data['사과'] = 700
 
-print(len(s))
+print(f"{len(data) + sum(data.values())}")
 
-# 4
+# 2204
