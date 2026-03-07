ac09f69229f499aa7e3e3e874d86f2c7e7525a74
sonsungjong <tbxmtbfm@naver.com>
2026-03-08 06:07:32 +0900

    update

--- src/app/resources/SQL/SQL2025_3_20.sql
+++ src/app/resources/SQL/SQL2025_3_20.sql
@@ -1,4 +1,5 @@
 CREATE TABLE A (colA INT, colB INT);
 INSERT INTO A VALUES (2, NULL), (3, 6), (2, 3), (NULL, 3), (4, 5);
 
+colA가 2 묶음(2,3) 혹은 colB가 3 묶음(3,5)인 데이터 중 colB가 NULL이 아닌 건수 조회
 SELECT count(colB) FROM A WHERE colA IN (2, 3) OR colB IN (3, 5);
